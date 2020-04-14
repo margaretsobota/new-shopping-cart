c1773f756c58fd81b1b7dffa825c8bf3e39f9c5a
Margot <margaretsobota@gmail.com>
2020-04-13 21:54:40 -0700

    styling of pictures

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import "rbx/index.css";
-import { Column, Container } from "rbx";
+import { Column, Container, Title } from "rbx";
+import "../style/card.css";
 //import test from "./public/data/product-imgs/101_1.jpg";
 
 const Card = ({product}) => {
-  const pic_url = process.env.PUBLIC_URL + "/data/product-imgs/";
+  const price = (product.price).toFixed(2);
   //<img src={pic_url + product.id + ".jpg"}/>
   //<img src={process.env.PUBLIC_URL + "/data/product-imgs/100_1.jpg"}/>
   
 
   return (
-    <Column size="one-quarter">
+    <Column id="card-column" size="one-fifth">
       <img src={process.env.PUBLIC_URL + "/data/product-imgs/" + product.sku + ".jpg"}/>
-      <h1>
+      <h1 id="card-title">
         { product.title }
       </h1>
-      <h3>
-        $ { product.price }
-      </h3>
+      <Title id="price" size={4}>
+        $ { price }
+      </Title>
     </Column>
   );
 };
